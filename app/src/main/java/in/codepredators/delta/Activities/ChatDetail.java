73cdb08d6f20314d68b231c73341865129e994df
Garima Singh <garingh128@gmail.com>
2020-01-08 00:57:46 +0530

    Chat details created.

---
+++ app/src/main/java/in/codepredators/delta/Activities/ChatDetail.java
@@ -0,0 +1,14 @@
+package in.codepredators.delta.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import in.codepredators.delta.R;
+
+public class ChatDetail extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.iotalkactivity_chatdetails);
+    }
+}
